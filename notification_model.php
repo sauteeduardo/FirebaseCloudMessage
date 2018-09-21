f7a81965d3174ed5dd285e8db0ff6167ef458e28
sauteeduardo <43472982+sauteeduardo@users.noreply.github.com>
2018-09-21 16:01:48 -0300

    First commit

---
+++ notification_model.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+class Notificacao_Model extends CI_Model {
+
+    private $api_key = "";
+
+    public function __construct(){
+
+        $this->load->library("fcm");
+
+    }
+
+    public function set($property,$value){
+
+        $this->{$property} = $value;
+
+    }
+
+    public function topico($topico,$notification = [],$data = []){
+
+        $this->fcm = new FCM(FCM::CONTENT_JSON, 60*60*24*5, null);
+
+        $this->fcm->set([
+
+            "api_key" => $this->api_key,
+
+            "url" => "https://fcm.googleapis.com/fcm/send"
+
+        ]);
+
+        $this->fcm->headers();
+
+        $this->fcm->message = [
+
+            "to" => "/topics/".$topico,
+
+            "notification" => $notification
+
+            "data" => $data
+
+        ];
+
+        return $this->fcm->send();
+
+    }
+
+}
